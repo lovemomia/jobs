4bceafb0253e35f9459411f0f3253a9134c13d29
吴秋锋 <arthas.wuqf@gmail.com>
2015-09-24 14:09:27 +0800

    重构jobs项目结构

--- src/main/java/cn/momia/jobs/order/Order.java
+++ src/main/java/cn/momia/jobs/order/Order.java
@@ -1,7 +1,6 @@
 package cn.momia.jobs.order;
 
 import java.io.Serializable;
-import java.math.BigDecimal;
 import java.util.List;
 
 public class Order implements Serializable {
@@ -14,11 +13,6 @@ public class Order implements Serializable {
         public static final int FINISHED = 4;  // 已完成
         public static final int TO_REFUND = 5; // 申请退款
         public static final int REFUNDED = 6;  // 已退款
-    }
-
-    public static final Order NOT_EXIST_ORDER = new Order();
-    static {
-        NOT_EXIST_ORDER.setId(0);
     }
 
     private long id;
@@ -65,35 +59,6 @@ public class Order implements Serializable {
 
     public void setPrices(List<OrderPrice> prices) {
         this.prices = prices;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Order)) return false;
-
-        Order order = (Order) o;
-
-        return getId() == order.getId();
-    }
-
-    @Override
-    public int hashCode() {
-        return (int) (getId() ^ (getId() >>> 32));
-    }
-
-    public boolean exists() {
-        return !this.equals(NOT_EXIST_ORDER);
-    }
-
-    public BigDecimal getTotalFee() {
-        BigDecimal totalFee = new BigDecimal(0);
-        for (OrderPrice price : prices) {
-            BigDecimal priceValue = price.getPrice().multiply(new BigDecimal(price.getCount()));
-            totalFee = totalFee.add(priceValue);
-        }
-
-        return totalFee;
     }
 
     public int getCount() {
