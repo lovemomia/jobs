62f75f02ab57053b4dfc05d594564dc6d38ee31d
吴秋锋 <arthas.wuqf@gmail.com>
2016-01-19 17:50:22 +0800

    add notifiers impl

---
+++ src/main/java/cn/momia/jobs/notify/impl/CouponExpiredNotifier.java
@@ -0,0 +1,11 @@
+package cn.momia.jobs.notify.impl;
+
+import cn.momia.common.service.AbstractService;
+import cn.momia.jobs.notify.Notifier;
+
+public class CouponExpiredNotifier extends AbstractService implements Notifier {
+    @Override
+    public void notifyUser() {
+
+    }
+}
---
+++ src/main/java/cn/momia/jobs/notify/impl/CourseFinishNotifier.java
@@ -0,0 +1,11 @@
+package cn.momia.jobs.notify.impl;
+
+import cn.momia.common.service.AbstractService;
+import cn.momia.jobs.notify.Notifier;
+
+public class CourseFinishNotifier extends AbstractService implements Notifier {
+    @Override
+    public void notifyUser() {
+
+    }
+}
